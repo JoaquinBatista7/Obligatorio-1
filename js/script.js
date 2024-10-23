c6836619443a5da03a32f8c1f0870080c3058488
JoaquinBatista7 <joaquinbatistadevelopment@gmail.com>
2024-10-23 16:11:25 -0300

    a

--- js/script.js
+++ js/script.js
@@ -1,73 +1,70 @@
 window.addEventListener("DOMContentLoaded", function () {
+  function artista(nombre, edad, estilo, artistas) {
+    this.nombre = nombre;
+    this.edad = edad;
+    this.estilo = estilo;
+    this.artistas = artistas;
+  }
 
-    function artista(nombre, edad, estilo, artistas) {
+  function exposicion(titulo, fecha, descripcion) {
+    this.titulo = titulo;
+    this.titulo = fecha;
+    this.descripcion = descripcion;
+  }
+  let colorCambiado = false; // Variable para rastrear el estado del color
+  let listaArtista = [];
+  let listaArtistaExpo = [];
 
-        this.nombre = nombre;
-        this.edad = edad;
-        this.estilo = estilo;
-        this.artistas = artistas;
-    }
+  document
+    .getElementById("btn-cambiar-colores")
+    .addEventListener("click", botonColores);
+  document
+    .getElementById("btn-agregar-artista")
+    .addEventListener("click", agregarArtista);
 
-    function exposicion(titulo, fecha, descripcion) {
+  function botonColores() {
+    // Color que se usará
+    const newColor = "#8DB58E";
 
-        this.titulo = titulo;
-        this.titulo = fecha;
-        this.descripcion = descripcion;
+    const originalColor = "#98FC98";
 
-    }
-    let colorCambiado = false; // Variable para rastrear el estado del color
-    let listaArtista = [];
-    let listaArtistaExpo = [];
+    // Cambiar el color según el estado actual de colorCambiado
+    document.getElementById("encabezado").style.backgroundColor = colorCambiado
+      ? originalColor
+      : newColor;
+    document.getElementById("seccion-ingresos").style.backgroundColor =
+      colorCambiado ? originalColor : newColor;
+    document.getElementById(
+      "seccion-informacion-general"
+    ).style.backgroundColor = colorCambiado ? originalColor : newColor;
 
-    document.getElementById("btn-cambiar-colores").addEventListener("click", botonColores);
-    document.getElementById("btn-agregar-artista").addEventListener("click", agregarArtista);
+    // Alternar el estado
+    colorCambiado = !colorCambiado; // Cambiar el estado
+  }
 
-    function botonColores() {
-        // Color que se usará
-        const newColor = '#8DB58E';
-        const originalColor = '#98FC98';
+  function agregarArtista() {
+    // Supongamos que el ID del select es "artistas-expo"
+    const select = document.getElementById("artistas-expo");
+    let nombre = document.getElementById("nombre-artista").value;
+    let edad = parseInt(document.getElementById("edad-artista").value);
+    let estilo = document.getElementById("estilo-artista").value;
+    // Crear un nuevo elemento <option>
+    const nuevaOpcion = document.createElement("option");
+    nuevaOpcion.text = nombre; // El texto que se mostrará
 
-        // Cambiar el color según el estado actual de colorCambiado
-        document.getElementById("encabezado").style.backgroundColor = colorCambiado ? originalColor : newColor;
-        document.getElementById("seccion-ingresos").style.backgroundColor = colorCambiado ? originalColor : newColor;
-        document.getElementById("seccion-informacion-general").style.backgroundColor = colorCambiado ? originalColor : newColor;
+    // Agregar el nuevo <option> al <select>
+    select.add(nuevaOpcion);
 
-        // Alternar el estado
-        colorCambiado = !colorCambiado; // Cambiar el estado
-    }
+    const nuevoArtista = new artista(nombre, edad, estilo);
+    listaArtista.push(nuevoArtista);
+  }
 
-
-
-    function agregarArtista() {
-
-        // Supongamos que el ID del select es "artistas-expo"
-        const select = document.getElementById("artistas-expo");
-        let nombre = document.getElementById("nombre-artista").value;
-        let edad = parseInt(document.getElementById("edad-artista").value);
-        let estilo = document.getElementById("estilo-artista").value;
-        // Crear un nuevo elemento <option>
-        const nuevaOpcion = document.createElement("option");
-        nuevaOpcion.text = nombre; // El texto que se mostrará
-
-
-        // Agregar el nuevo <option> al <select>
-        select.add(nuevaOpcion);
-
-        const nuevoArtista = new artista(nombre, edad, estilo);
-        listaArtista.push(nuevoArtista);
-
-    }
-
-    function agregarExposicion() {
-
-        const select = document.getElementById("artistas-expo2");
-        let titulo = document.getElementById("titulo-expo").value;
-        let fecha = parseInt(document.getElementById("fecha-expo").value);
-        let descripcion = document.getElementById("descripcion-expo").value;
-
-
-    }
-
-
-
+  function agregarExposicion() {
+    const select = document.getElementById("artistas-expo2");
+    let titulo = document.getElementById("titulo-expo").value;
+    let fecha = parseInt(document.getElementById("fecha-expo").value);
+    let descripcion = document.getElementById("descripcion-expo").value;
+  }
 });
+
+
